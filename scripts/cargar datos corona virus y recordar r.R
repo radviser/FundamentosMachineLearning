cd9eb58ca684e7000d013c8ad52abe296ebecd8a
radviser <mrochareynoso@gmail.com>
2020-02-28 19:05:10 -0600

    sigo probando esto

--- scripts/cargar datos corona virus y recordar r.R
+++ scripts/cargar datos corona virus y recordar r.R
@@ -11,3 +11,27 @@ library(readr)
 print ("cargar datos")
 
 print ("probando")
+
+# Cargar los datos
+datos <- read.csv("datos/covid_19_data.csv")
+datos
+
+summary(datos)
+str(datos)
+
+unique(datos$Country.Region)
+
+
+# ¿Cuángos casos confirmados ?
+sum(datos$Confirmed)
+
+# ¿cántos desscesos?
+sum(datos$Deaths)
+
+# ¿Porcentaje, descesos confirmados
+sum(datos$Confirmed / sum(datos$Deaths))
+
+paste(round(sum(datos$Confirmed / sum(datos$Deaths)),2), " % ")
+
+
+print("Cambios")
